044c4ecb047e8187525d230f51085098b084e82c
Idamsyach <idamsyach@gmail.com>
2026-03-18 12:57:25 +0700

    Updates

---
+++ assets/8sV4XVe5.js
@@ -0,0 +1,1 @@
+import{r as n}from"./DQawvsgc.js";import a from"./BXuHn9Jf.js";import{a7 as s,K as t,o as e,p as i,N as p}from"./CqAfcf7p.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
---
+++ assets/CTNlCHbn.js
@@ -0,0 +1,1 @@
+import{be as r}from"./CqAfcf7p.js";function s(t,e="reka"){return`${e}-${r?.()}`}export{s as u};
